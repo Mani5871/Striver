567d8b2d40cfe648b65ced325229314f6cfb6415
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-06-08 10:53:19 +0530

    Create Duplicate Number Efficient .cpp

---
+++ DAY01/Duplicate Number Efficient .cpp
@@ -0,0 +1,32 @@
+// Time Complexity : O(N)
+// Space Complexity : O(1)
+// https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int slow, fast;
+        
+        slow = nums[0];
+        fast = nums[0];
+        
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        while(slow != fast);
+        
+        slow = nums[0];
+        
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        
+        return slow;
+    }
+};
