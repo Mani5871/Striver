308e380da77f95a8440de9c706cb359468eec1e9
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2021-09-15 11:00:31 +0530

    Update PreOrder Iterative.cpp

--- DAY17/PreOrder Iterative.cpp
+++ DAY17/PreOrder Iterative.cpp
@@ -2,27 +2,31 @@
 // Space Complexity : O(N)
 // https://practice.geeksforgeeks.org/problems/preorder-traversal/1#
 
-vector <int> preorder(Node* root)
-{
-  // Your code here
-  vector<int> val;
-  stack<Node*> ord;
-  Node * curr = root;
-  
-  while(ord.empty() == false || curr != NULL)
-  {
-    //   val.push_back(curr -> data);
-      
-      while(curr != NULL)
-      {
-          val.push_back(curr -> data);
-          ord.push(curr);
-          curr = curr -> left;
-      }
-      
-      curr = ord.top();
-      ord.pop();
-      curr = curr -> right;
-  }
-  
-  return val;
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+
+        vector<int> nodes;
+
+        if(!root) return nodes;
+
+        stack <TreeNode*> st; 
+        st.push(root);
+
+        while(!st.empty())
+        {
+            root = st.top();
+            nodes.push_back(root -> val);
+            st.pop();
+
+            if(root -> right)
+                st.push(root -> right);
+
+            if(root -> left)
+                st.push(root -> left);
+        }
+        
+        return nodes;
+    }
+};
+
