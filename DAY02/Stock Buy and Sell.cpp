f5488a1002c1a3047db5a9737ec58fb22deefb8b
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2022-03-30 20:35:45 +0530

    Update Stock Buy and Sell.cpp

--- DAY02/Stock Buy and Sell.cpp
+++ DAY02/Stock Buy and Sell.cpp
@@ -1,48 +1,17 @@
 // Time Complexity : O(N)
 // Space Complexity : O(1)
-// https://practice.geeksforgeeks.org/problems/stock-buy-and-sell-1587115621/1
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
-class Solution{
+class Solution {
 public:
-    vector<vector<int> > stockBuySell(vector<int> a, int n){
-        // code here
-        vector<vector<int>> result;
-        int i, bi = 0, si, buy = a[0], sell = -1;
-        
-        for(i = 1; i < n; i ++)
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0], profit = 0, sell = 0;
+        for(int i = 1; i < prices.size(); i++)
         {
-            if(a[i] >= sell && a[i] > buy)
-            {
-                sell = a[i];
-                si = i;
-            }
-            
-
-            if(a[i] < sell || a[i] <= buy)
-            {
-                
-                if(sell != -1)
-                {
-                    result.push_back({bi, si});
-                    buy = a[i];
-                    bi = i;
-                    sell = -1;
-                    
-                }
-                if(a[i] <= buy)
-                {
-                    
-                    buy = a[i];
-                    bi = i;
-                    sell = -1;
-                }
-                
-            }
+            profit = max(prices[i] - buy, profit);
+            if(prices[i] < buy)
+                buy = prices[i];
         }
-        if(buy < sell)
-            result.push_back({bi, si});
-        
-        return result;
-        
+        return profit;
     }
 };
