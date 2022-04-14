523ca9d7c3f5bb8ea34967a38c86f4178bc8e674
Mani5871 <58336312+Mani5871@users.noreply.github.com>
2022-04-14 10:51:27 +0530

    Create Search in Rotated Array.cpp

---
+++ DAY11/Search in Rotated Array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int low = 0, high = nums.size() - 1;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            
+            if(target == nums[mid])
+                return mid;
+            
+            if(nums[low] <= nums[mid])
+            {
+                if(nums[low] <= target and target <= nums[mid])
+                    high = mid - 1;
+                
+                else
+                    low = mid + 1;
+            }
+            
+            else
+            {
+                if(nums[mid] <= target and target <= nums[high])
+                    low = mid + 1;
+                
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
